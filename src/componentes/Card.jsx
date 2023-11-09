1ecde5dd50fb908a201251521cce613a42255845
Alvaro Mamani <alvarogabriel@outlook.com.ar>
2023-11-09 01:52:30 -0300

    incorporacion de la api

--- src/componentes/Card.jsx
+++ src/componentes/Card.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({ book }) => {
+  console.log(book);
   return (
     <>
-      <div className="card">
-        <img src="./react-book.png" alt="Portada de libro" />
-        <div className="bottom">
-          <h3 className="title">React Js</h3>
-          <p className="amount">&#8377;3290</p>
-        </div>
-      </div>
+      {
+        book.map((item) => {
+          let thumbnail=item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
+          if (thumbnail!= undefined){
+            return (
+            <>
+              <div className="card">
+                <img src={thumbnail} alt="Portada de libro" />
+                <div className="bottom">
+                  <h3 className="title">React Js</h3>
+                  <p className="amount">&#8377;3290</p>
+                </div>
+              </div>
+            </>
+          )
+        }
+          
+        })
+      }
     </>
-
   )
 }
 
