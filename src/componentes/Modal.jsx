5f37c7b8eba99a245b67f888df9a8c3cd3bfb64d
Alvaro Mamani <alvarogabriel@outlook.com.ar>
2023-11-13 23:12:34 -0300

    propiedades show, item y onclose, props: img, title..

--- src/componentes/Modal.jsx
+++ src/componentes/Modal.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 
-const Modal = () => {
+const Modal = ({show, item, onClose}) => {
+  if(!show){
+    return null;
+  }
+  let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
   return (
     <>
       <div className='overlay'>
         <div className="overlay-inner">
-          <button className='close'><i class="bi bi-x-lg"></i></button>
+          <button className='close' onClick={onClose}><i class="bi bi-x-lg"></i></button>
           <div className="inner-box">
-            <img src="./react-book.png" alt="Portada del libro" />
+            <img src={thumbnail} alt="Portada del libro" />
             <div className="info">
-              <h1>React by Example - building modern web applications with React</h1><br />
-              <h3>Prathamesh Sonopatki</h3>
-              <h4>Packt Publishing Ltd <span>2016-04-21</span></h4><br />
-              <a href="#"><button>More</button></a>
+              <h1>{item.volumeInfo.title}</h1><br />
+              <h3>{item.volumeInfo.authors}</h3>
+              <h4>{item.volumeInfo.publisher}<span>{item.volumeInfo.publishedData}</span></h4><br />
+              <a href={item.volumeInfo.previewLink} ><button>More</button></a>
             </div>
           </div>
-          <h4 className='description'>Knowledge for free... get that job, you aspired Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, officia? Animi, veniam optio libero beatae a culpa impedit suscipit porro adipisci quis. Tenetur tempora ipsa autem porro explicabo rem vel.</h4>
+          <h4 className='description'>{item.volumeInfo.description}</h4>
         </div>
       </div>
     </>
