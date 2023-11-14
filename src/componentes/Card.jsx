7c9c1aaadd987ddc31dc82b56ccf6453580c581b
Alvaro Mamani <alvarogabriel@outlook.com.ar>
2023-11-13 23:10:33 -0300

    se agrgo useState, funciones a botones y a la card

--- src/componentes/Card.jsx
+++ src/componentes/Card.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from './Modal';
 
 const Card = ({ book }) => {
-  console.log(book);
+  // console.log(book);
+  const [show, setShow] = useState(false);
+  const [bookItem, setBookItem] = useState();
   return (
     <>
       {
         book.map((item) => {
-          let thumbnail=item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
-          if (thumbnail!= undefined){
+          let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
+          let amount = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
+          if (thumbnail !== undefined && amount !== undefined) {
             return (
-            <>
-              <div className="card">
-                <img src={thumbnail} alt="Portada de libro" />
-                <div className="bottom">
-                  <h3 className="title">React Js</h3>
-                  <p className="amount">&#8377;3290</p>
+              <>
+                <div className="card" onClick={()=> {setShow(true); setBookItem(item)}}>
+                  <img src={thumbnail} alt="Portada de libro" />
+                  <div className="bottom">
+                    <h3 className="title">{item.volumeInfo.title}</h3>
+                    <p className="amount">&#8377;{amount}</p>
+                  </div>
                 </div>
-              </div>
-            </>
-          )
-        }
-          
+                <Modal show={show} item={bookItem} onClose={()=>setShow(false)} />
+              </>
+            )
+          }
+
         })
       }
     </>
